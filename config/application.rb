8e1a9e2306d5c325d22dfa1d1e0babfc6ff4b44c
Hijas <hijas@foradian.com>
2021-02-06 07:37:52 +0530

    added rake task 'rake red_hat:update_currency_rates' to update currency rate by crontab job, loading all lib sub directories in application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module RecruitingCodingExercise
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # this will load the lib directory
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]
   end
 end
